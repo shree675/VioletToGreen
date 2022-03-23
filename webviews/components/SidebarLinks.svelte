69cbafbe4b49b8b454383492a1f64d4e06fe2932
Shreetesh M <shreeteshm37@gmail.com>
2022-03-23 14:08:41 +0530

    added tooltips and diagnostics placeholder

--- webviews/components/SidebarLinks.svelte
+++ webviews/components/SidebarLinks.svelte
@@ -55,6 +55,14 @@
     return str;
   };
 
+  const formatPath = (val: string) => {
+    let str = val;
+    if (str.length > 25) {
+      str = " ..." + val.substring(val.lastIndexOf("\\") - 10, val.length);
+    }
+    return str;
+  };
+
   onMount(() => {
     window.addEventListener("message", (event) => {
       const message = event.data;
@@ -90,6 +98,7 @@
     <div>
       <div
         class="blue"
+        data-tooltip="Path: {formatPath(item[0].filepath)}"
         on:click={() => {
           tsvscode.postMessage({
             type: "gotoLine",
@@ -109,6 +118,7 @@
       </div>
       <div
         class="green"
+        data-tooltip="Path: {formatPath(item[1].filepath)}"
         on:click={() => {
           tsvscode.postMessage({
             type: "gotoLine",
@@ -161,5 +171,67 @@
   }
   .line-number {
     font-size: 10px;
+  }
+  [data-tooltip] {
+    position: relative;
+    z-index: 2;
+    display: block;
+  }
+
+  [data-tooltip]:before,
+  [data-tooltip]:after {
+    visibility: hidden;
+    opacity: 0;
+    pointer-events: none;
+    transition: 0.2s ease-out;
+    transform: translate(-50%, 5px);
+  }
+
+  [data-tooltip]:before {
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    margin-bottom: 5px;
+    padding: 7px;
+    width: 100%;
+    min-width: 70px;
+    max-width: 250px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    background-color: #000;
+    background-color: hsla(0, 10%, 10%, 0.9);
+    color: #fff;
+    content: attr(data-tooltip);
+    text-align: center;
+    font-size: 12px;
+    line-height: 1.2;
+    transition: 0.2s ease-out;
+  }
+
+  [data-tooltip]:after {
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    width: 0;
+    border-top: 5px solid #000;
+    border-top: 5px solid hsla(0, 0%, 20%, 0.9);
+    border-right: 5px solid transparent;
+    border-left: 5px solid transparent;
+    content: " ";
+    font-size: 0;
+    line-height: 0;
+  }
+
+  [data-tooltip]:hover:before,
+  [data-tooltip]:hover:after {
+    visibility: visible;
+    opacity: 1;
+    transform: translate(-50%, 0);
+  }
+  [data-tooltip="false"]:hover:before,
+  [data-tooltip="false"]:hover:after {
+    visibility: hidden;
+    opacity: 0;
   }
 </style>
