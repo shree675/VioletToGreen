8995e75e6a287dd991590ff1033f4d7b90a94062
Kranthi <skranthi4444@gmail.com>
2022-03-28 19:05:49 +0530

    update fields of comments object

--- src/parser.ts
+++ src/parser.ts
@@ -1,62 +1,112 @@
 import * as vscode from "vscode";
 
+// types of comments
 enum CommentType {
   inline,
   singleLine,
   multiline,
 }
 
+/**
+ * A function that returns the line number of the matched comment
+ * @param text full text of the document
+ * @param index the matched index
+ * @returns the line number in the document
+ */
+const getLine = (text: string, index: number) => {
+  const numNewLines = text.slice(0, index).split("\n").length;
+  return numNewLines;
+};
+
+
+/**
+ * A function that returns the column number of the matched comment
+ * @param text full text of the document
+ * @param index the matched index
+ * @returns the line number in the document
+ */
+const getColumn = (text: string, index: number) => {
+  const numNewLines = text.slice(0, index).split("\n").length - 1;
+  const numSpaces = text.slice(0, index).split("\n")[numNewLines].length;
+  return numSpaces + 1;
+};
+
+const TAG = "COMMENTS";
+
+
 export function linkComments(editor?: vscode.TextEditor) {
   if (!editor) {
     return;
   }
 
-  const singleLineComment = /\/\/.*/g;
-  const multilineComment = /\/\*[\s\S]*?\*\//gm;
+  const singleLinedComments = /[/]{2}.*(?:(?:\r\n|\r|\n) *[/].*)*/gm;
+  const multilinedComment = /\/\*[\s\S]*?\*\//gm;
+
   const text = editor.document.getText();
   const lines = text.split("\n");
+
   const comments: Array<{
-    fullLine?: string;
-    lineNumber: Number;
     comment: string;
     type: CommentType;
-    // startCharacter: Number;
-    // startLine: Number;
-    // endCharacter: Number;
-    // endLine: Number;
+    startCharacter: Number;
+    startLine: Number;
+    endCharacter: Number;
+    endLine: Number;
   }> = [];
 
-  const matches = text.matchAll(multilineComment);
-  for (const match in matches) {
+  const multiLineMatches = text.matchAll(multilinedComment);
+  for (const match of multiLineMatches) {
+
+    // TODO: match.index will not be null
+    // because we are having a global flag in the pattern
+    // but make sure to try to come up with cases where this can
+    // be null
+
+    const startLine = getLine(text, match.index!);
+    const endLine = getLine(text, match.index! + match[0].length);
+
+    const startCharacter = getColumn(text, match.index!);
+    const endCharacter = getColumn(
+      text,
+      match.index! + match[0].length
+    );
+
     comments.push({
-        comment: match[0],
-        lineNumber: 0,
-        type: CommentType.multiline,
+      comment: match[0],
+      startCharacter,
+      startLine,
+      endCharacter,
+      endLine,
+      type: CommentType.multiline,
     });
   }
 
-    for (const line of lines) {
-      //   check if the comment is a single line comment
-      if (singleLineComment.test(line)) {
-        // check if the comment is a single-line comment
-          if (line.trim().startsWith("//")) {
-            comments.push({
-              fullLine: line,
-              lineNumber: lines.indexOf(line) + 1,
-              comment: line.match(singleLineComment)![0],
-              type: CommentType.singleLine,
-            });
-          } else {
-            // check if the comment is an inline comment
-            comments.push({
-              fullLine: line,
-              lineNumber: lines.indexOf(line) + 1,
-              comment: line.match(singleLineComment)![0],
-              type: CommentType.inline,
-            });
-          }
-      }
-    }
-    console.log("Comments:", comments);
-}
+  const singleLineMatches = text.matchAll(singleLinedComments);
+  for (const match of singleLineMatches) {
+    const startLine = getLine(text, match.index!);
+    const endLine = getLine(text, match.index! + match[0].length);
 
+    const startCharacter = getColumn(text, match.index!);
+    const endCharacter = getColumn(
+      text,
+      match.index! + match[0].length
+    );
+
+    let commentType = CommentType.inline;
+
+    if (lines[startLine - 1].trim().startsWith("//")) {
+      commentType = CommentType.singleLine;
+    }
+
+    comments.push({
+      comment: match[0],
+      startCharacter,
+      startLine,
+      endCharacter,
+      endLine,
+      type: commentType,
+    });
+  }
+
+  console.log(TAG, comments);
+}
