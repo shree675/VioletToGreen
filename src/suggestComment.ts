996c4f24cccfe0b2a2fec72b7c5711b9467a2a5e
Debeshee <debesheedas@gmail.com>
2022-04-09 20:22:17 +0530

    refactored readabilityMetrics and created suggestComments script

---
+++ src/suggestComment.ts
@@ -0,0 +1,12 @@
+import * as vscode from "vscode";
+import { runHeuristics } from "./heuristics";
+
+function suggestComments(javaText: string, uri: string, configPath: string) {
+
+    //just read from config file
+
+    //links = 
+    
+}
+
+export default suggestComments;
